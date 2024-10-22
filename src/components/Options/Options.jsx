9045b38012719337599a5c6b54281c974f3430c5
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-22 13:06:03 +0300

    update

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,11 +2,11 @@ import css from './Options.module.css'
 
 export default function Options({ onLeaveFeedback, resetFeedback, totalFeedback }) {
   return (
-    <div className= {css.button_wrapper}>
-      <button className= {`${css.button_feedback} ${css.good}`} onClick={() => onLeaveFeedback('good')}>Good</button>
-      <button className= {`${css.button_feedback} ${css.neutral}`} onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
-      <button className= {`${css.button_feedback} ${css.bad}`} onClick={() => onLeaveFeedback('bad')}>Bad</button>
-      {totalFeedback > 0 && <button className= {`${css.button_feedback} ${css.reset}`} onClick={() => resetFeedback()}>Reset</button>}
-    </div>
+    <ul className= {css.button_wrapper}>
+      <li><button className= {`${css.button_feedback} ${css.good}`} onClick={() => onLeaveFeedback('good')}>Good</button></li>
+      <li><button className= {`${css.button_feedback} ${css.neutral}`} onClick={() => onLeaveFeedback('neutral')}>Neutral</button></li>
+      <li><button className= {`${css.button_feedback} ${css.bad}`} onClick={() => onLeaveFeedback('bad')}>Bad</button></li>
+      {totalFeedback > 0 && <li><button className= {`${css.button_feedback} ${css.reset}`} onClick={() => resetFeedback()}>Reset</button></li>}
+    </ul>
   )
 }
