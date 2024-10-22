f29485ed9755f782f75dc7afd2edaa76a31a3375
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-22 14:01:34 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react';
-import './App.css'
-import Description from './Description/Description'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Description from './Description/Description';
 import Options from './Options/Options';
 import Feedback from './Feedback/Feedback';
 import Notification from './Notification/Notification';
 
 function App() {
-
   const [feedback, setFeedback] = useState(() => {
-
     const savedFeedback = localStorage.getItem('feedback');
-
     return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
   });
 
-const {good, neutral , bad} = feedback
 
-const totalFeedback = good + neutral + bad;
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
-
+  const { good, neutral, bad } = feedback;
+  const totalFeedback = good + neutral + bad;
 
   const updateFeedback = feedbackType => {
-    setFeedback(prevFeedback => {
-      const updatedFeedback = {
-        ...prevFeedback,
-        [feedbackType]: prevFeedback[feedbackType] + 1,
-      };
-      localStorage.setItem('feedback', JSON.stringify(updatedFeedback));
-      return updatedFeedback;
-    });
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
   };
 
   const resetFeedback = () => {
@@ -37,20 +32,17 @@ const totalFeedback = good + neutral + bad;
       neutral: 0,
       bad: 0,
     });
-    localStorage.removeItem('feedback')
   };
-
 
   return (
     <div className='container'>
       <Description />
-      <Options onLeaveFeedback={updateFeedback} resetFeedback ={resetFeedback} totalFeedback = {totalFeedback} />
+      <Options onLeaveFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
       {totalFeedback > 0
-          ? <Feedback valueFeedBack={ feedback } totalFeedback ={totalFeedback} />
-          : <Notification/>}
+        ? <Feedback valueFeedBack={feedback} totalFeedback={totalFeedback} />
+        : <Notification />}
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
